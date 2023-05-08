915692320d64592001332dd34d0e7c9eb680d7d6
rivashkin <rivashkin@naumen.ru>
2023-05-08 17:56:49 +0500

    http final

--- src/main/scala/director/Director.scala
+++ src/main/scala/director/Director.scala
@@ -16,17 +16,26 @@ trait Director {
 
     private val scripts: Seq[() => IO[Unit]] = List(
         () => startP2PScript(),
-        () => startBroadcastScript(),
-        () => startRedirectScript()
+        () => startRedirectScript(),
+        () => startBroadcastScript()
     )
     def startP2PScript(): IO[Unit]
     def startBroadcastScript(): IO[Unit]
     def startRedirectScript(): IO[Unit]
 
+    private def getScript(): IO[() => IO[Unit]] = {
+        val seed = Random.between(1, 101)
+        val script = seed match {
+            case s if s <= 60 => scripts(0)
+            case s if (60 < s) && (s <= 80) => scripts(1)
+            case _ => scripts(2)
+        }
+        IO(script)
+    }
+
     def startRandomWorks(): IO[Unit] = {
         for {
-            number <- IO(Random.between(0, scripts.length))
-            script <- IO(scripts(number))
+            script <- getScript()
             _      <- script()
             _      <- IO.sleep(config.generationInfo.sendDurationSec.seconds)
         } yield ()
